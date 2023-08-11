d8d629c39b569680add91f5b57cc99ab820dbbf7
clach04 <clach04@gmail.com>
2023-08-10 19:50:26 -0700

    Derived filename support for export

--- simplenote_export2txt.py
+++ simplenote_export2txt.py
@@ -6,7 +6,10 @@
 # Copyright (C) 2023 Chris Clark - clach04
 """Only dumps active notes, not trashed/deleted notes
 
-Uses id as filename to avoid duplicates and potentially invalid names (too long, unsupported characters, etc.).
+Can:
+
+  * uses id as filename to avoid duplicates and potentially invalid names (too long, unsupported characters, etc.).
+  * use first line of note as filename, note file name will be cleaned/filtered/made-safe
 
 Possible future TODO items:
 
@@ -48,6 +51,9 @@ except ImportError:
     windows_set_create_time = None
 
 
+import sanity_check_export
+
+
 def safe_mkdir(newdir):
     result_dir = os.path.abspath(newdir)
     try:
@@ -65,14 +71,26 @@ def iso_like2datetime(datetime_str):
     datetime_str = datetime_str.split('.', 1)[0]  # strip fractional seconds and UTC indicator
     return datetime.datetime.strptime(datetime_str, '%Y-%m-%dT%H:%M:%S')
 
-def dict2txt(notes_dict, output_directory='notes_export_dir', use_first_line_as_filename=False):
-    assert use_first_line_as_filename == False  # dupe detection and resolution needed if True
+def dict2txt(notes_dict, output_directory='notes_export_dir', use_first_line_as_filename=False, file_extension='txt'):
+    #import pdb ; pdb.set_trace()
+    if use_first_line_as_filename:
+        dupe_dict = sanity_check_export.find_duplicate_filenames_dict(notes_dict, generate_file_name=sanity_check_export.safe_filename)
+
     safe_mkdir(output_directory)
     notes = {}
     for note_entry in notes_dict['activeNotes']:
         # handle platform format differences with newlines/linefeeds
         note_entry['content'] = note_entry['content'].replace('\r', '')  # I don't use an Apple Mac, I've no idea if this will break OS X - works for Windows, Linux, and Android
         filename = note_entry['id']
+        if use_first_line_as_filename:
+            first_line = note_entry['content'].split('\n', 1)[0]
+            filename = sanity_check_export.safe_filename(first_line)
+            if filename.lower() in dupe_dict:
+                filename = 'dupe__' + filename.lower() + '__' + note_entry['id']  # TODO review if should use lower in generated final name for dupes?
+
+        # TODO check for markdown and potentially change/set file_extension?
+        if file_extension:
+            filename = filename + '.' + file_extension
         filename = os.path.join(output_directory, filename)
         st_atime = time.time()  # current time for; Time of most recent access expressed in seconds.
         last_modified = iso_like2datetime(note_entry['lastModified'])
@@ -111,7 +129,7 @@ def main(argv=None):
         json_bytes = f.read()
         f.close()
         notes_dict = json.loads(json_bytes)
-    dict2txt(notes_dict, output_directory=filename+'_dir')
+    dict2txt(notes_dict, output_directory=filename+'_dir', use_first_line_as_filename=True)
 
 
     return 0
