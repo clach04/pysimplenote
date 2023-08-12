97b183516080a66e41b837ba60947d4a96373374
clach04 <clach04@gmail.com>
2023-08-12 14:21:05 -0700

    import_files_to_git.py import txt files into git 1-by-1

---
+++ import_files_to_git.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: us-ascii -*-
+# vim:ts=4:sw=4:softtabstop=4:smarttab:expandtab
+#
+# For each *.txt file in directory create git add/commit script that uses the file last modified date/time to commit to a git repo (in time order)
+# Copyright (C) 2023 Chris Clark - clach04
+
+import datetime
+import json
+import glob
+import os
+import string
+import sys
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    print('# Python %s on %s' % (sys.version.replace('\n', ' '), sys.platform.replace('\n', ' ')))
+    filename_list = glob.glob('*.txt')
+    times_and_filenames = []
+    for filename in filename_list:
+        #print('%r' % filename)
+        fd = os.open(filename, os.O_RDONLY)
+        file_status = os.fstat(fd)
+        os.close(fd)
+        times_and_filenames.append((file_status.st_mtime, filename, ))
+
+    times_and_filenames.sort()  # sort by timestamp
+    #print('%s' % json.dumps(times_and_filenames, indent=4))  # DEBUG
+    # Generate shell / batch script to stdout - assume now unicode filenames for Windows
+    print('git init')
+    for timestamp, filename in times_and_filenames:
+        command_str = '''git add "%s"
+git commit -m "%s" --date=%d
+'''
+        print(command_str % (filename, filename, timestamp))
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
